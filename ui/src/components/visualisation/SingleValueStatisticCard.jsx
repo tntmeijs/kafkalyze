96fe814bafdd7c94fe8557c007e59f11dc9a6702
Tahar Meijs <tntmeijs@gmail.com>
2023-10-31 08:46:55 +0100

    feat: add loader for single value statistic card component

--- ui/src/components/visualisation/SingleValueStatisticCard.jsx
+++ ui/src/components/visualisation/SingleValueStatisticCard.jsx
@@ -1,9 +1,20 @@
-export const SingleValueStatisticCard = ({ title, value, unit, colour }) => (
-    <div className="card">
-        <div className={`card-content highlight-bottom is-uppercase ${colour}`}>
+export const SingleValueStatisticCard = ({ title, value, unit, colour, loading, wrapperClassName }) => (
+    <div className={`card highlight-bottom ${colour} ${wrapperClassName}`}>
+        <div className="card-content is-uppercase">
             <div className="content">
-                <p className="subtitle is-6 mb-5">{title}</p>
-                <p className="title is-4"><span className="mr-2">{value}</span><span className="has-text-weight-light">{unit}</span></p>
+                <p className="subtitle is-6">{title}</p>
+                {loading
+                    ? (
+                        <div>
+                            <progress class={`progress is-small ${colour}`}></progress>
+                        </div>
+                    )
+                    : (
+                        <p className="title is-4 pt-2">
+                            <span className="mr-2">{value}</span>
+                            <span className="has-text-weight-light">{unit}</span>
+                        </p>
+                    )}
             </div>
         </div>
     </div>
--- ui/src/pages/OverviewPage.jsx
+++ ui/src/pages/OverviewPage.jsx
@@ -1,22 +1,34 @@
+import { useEffect, useState } from "react";
 import { AreaChart } from "../components/visualisation/AreaChart";
 import { DoughnutChart } from "../components/visualisation/DoughnutChart";
 import { SingleValueStatisticCard } from "../components/visualisation/SingleValueStatisticCard";
 
 export const OverviewPage = () => {
+    const [statistics, setStatistics] = useState({
+        eventsPerHourCount: null,
+        totalEventCount: null,
+        databaseSize: null,
+        topicCount: null
+    });
+
+    useEffect(() => {
+        // TODO: fetch statistics from backend
+    }, []);
+
     return (
         <div className="box">
             <div className="columns">
-                <div className="column">
-                    <SingleValueStatisticCard title="events per hour" value="18.000" colour="is-info" />
+                <div className="column is-flex is-flex-direction-column">
+                    <SingleValueStatisticCard title="events per hour" value={statistics.eventsPerHourCount} loading={!statistics.eventsPerHourCount} colour="is-info" wrapperClassName="is-flex-grow-1" />
                 </div>
-                <div className="column">
-                    <SingleValueStatisticCard title="total events stored" value="7.500.000" colour="is-danger" />
+                <div className="column is-flex is-flex-direction-column">
+                    <SingleValueStatisticCard title="total events stored" value={statistics.totalEventCount} loading={!statistics.totalEventCount} colour="is-danger" wrapperClassName="is-flex-grow-1" />
                 </div>
-                <div className="column">
-                    <SingleValueStatisticCard title="database size" value={12.6} unit="GB" colour="is-warning" />
+                <div className="column is-flex is-flex-direction-column">
+                    <SingleValueStatisticCard title="database size" value={statistics.databaseSize} loading={!statistics.databaseSize} unit="GB" colour="is-warning" wrapperClassName="is-flex-grow-1" />
                 </div>
-                <div className="column">
-                    <SingleValueStatisticCard title="number of topics" value={236} colour="is-success" />
+                <div className="column is-flex is-flex-direction-column">
+                    <SingleValueStatisticCard title="number of topics" value={statistics.topicCount} loading={!statistics.topicCount} colour="is-success" wrapperClassName="is-flex-grow-1" />
                 </div>
             </div>
 
