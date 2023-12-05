63a256a094d8a72c3e25c2bfcada64f6848db84e
Tahar Meijs <tntmeijs@gmail.com>
2023-12-05 09:26:32 +0100

    feat: add new endpoint to query event store database

--- server/src/main/java/dev/tahar/server/controller/EventsController.java
+++ server/src/main/java/dev/tahar/server/controller/EventsController.java
@@ -3,6 +3,7 @@ package dev.tahar.server.controller;
 import dev.tahar.server.service.ProduceEventService;
 import lombok.RequiredArgsConstructor;
 import org.openapitools.api.EventsApi;
+import org.openapitools.model.EventStoreQueryResultV1;
 import org.openapitools.model.ProduceStringEventV1;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,14 @@ public class EventsController implements EventsApi {
     @Override
     public ResponseEntity<Void> produceStringEventV1(ProduceStringEventV1 produceStringEventV1) {
         produceEventService.produceToTopic(produceStringEventV1.getData(), produceStringEventV1.getTopic());
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ResponseEntity<EventStoreQueryResultV1> queryEventStoreV1(String query) {
         return ResponseEntity.ok().build();
     }
 
